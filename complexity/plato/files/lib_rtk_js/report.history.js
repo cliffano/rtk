d81df5f84661a8d0617616b4e9cb42156d853cac
cliffano <cliffano@users.noreply.github.com>
2021-08-11 12:09:40 +0000

    deploy: 7a9bde755b2f71670de90566d3bfaef7b37502b2

--- complexity/plato/files/lib_rtk_js/report.history.js
+++ complexity/plato/files/lib_rtk_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 11 Aug 2021 12:08:40 GMT","sloc":83,"lloc":32,"functions":7,"deliveredBugs":0.437,"difficulty":21.635,"maintainability":73.844,"lintErrors":2}]
+__history = [{"date":"Wed, 11 Aug 2021 12:09:36 GMT","sloc":83,"lloc":32,"functions":7,"deliveredBugs":0.437,"difficulty":21.635,"maintainability":73.844,"lintErrors":2}]
--- complexity/plato/report.history.js
+++ complexity/plato/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 11 Aug 2021 12:08:40 GMT","total":{"sloc":586,"maintainability":679.315},"average":{"sloc":65,"maintainability":"75.48"}}]
+__history = [{"date":"Wed, 11 Aug 2021 12:09:36 GMT","total":{"sloc":586,"maintainability":679.315},"average":{"sloc":65,"maintainability":"75.48"}}]
