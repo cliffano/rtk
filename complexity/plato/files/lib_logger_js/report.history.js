4cedba42502d19b02018e6d026461c23788ed5f2
cliffano <cliffano@users.noreply.github.com>
2021-08-11 06:31:42 +0000

    deploy: f52912c3efde0afc71cd2e0476460eb3af2813d1

--- complexity/plato/files/lib_logger_js/report.history.js
+++ complexity/plato/files/lib_logger_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 11 Aug 2021 06:31:35 GMT","sloc":29,"lloc":8,"functions":2,"deliveredBugs":0.073,"difficulty":10.214,"maintainability":92.61,"lintErrors":4}]
+__history = [{"date":"Wed, 11 Aug 2021 06:31:37 GMT","sloc":29,"lloc":8,"functions":2,"deliveredBugs":0.073,"difficulty":10.214,"maintainability":92.61,"lintErrors":4}]
