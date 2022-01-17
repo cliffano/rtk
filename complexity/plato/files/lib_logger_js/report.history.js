74e4bbfde1aca7f507fe2e83e52f2ac8beff0e39
cliffano <cliffano@users.noreply.github.com>
2022-01-17 10:55:20 +0000

    deploy: 446d2c48d1eaaa4ffd91cbc475b471ed44d89ca8

--- complexity/plato/files/lib_logger_js/report.history.js
+++ complexity/plato/files/lib_logger_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 17 Jan 2022 10:54:28 GMT","sloc":29,"lloc":8,"functions":2,"deliveredBugs":0.073,"difficulty":10.214,"maintainability":92.61,"lintErrors":4}]
+__history = [{"date":"Mon, 17 Jan 2022 10:55:16 GMT","sloc":29,"lloc":8,"functions":2,"deliveredBugs":0.073,"difficulty":10.214,"maintainability":92.61,"lintErrors":4}]
--- complexity/plato/files/lib_version_schemes_semver_js/report.history.js
+++ complexity/plato/files/lib_version_schemes_semver_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 17 Jan 2022 10:54:29 GMT","sloc":45,"lloc":15,"functions":3,"deliveredBugs":0.262,"difficulty":21.661,"maintainability":70.494,"lintErrors":2}]
+__history = [{"date":"Mon, 17 Jan 2022 10:55:16 GMT","sloc":45,"lloc":15,"functions":3,"deliveredBugs":0.262,"difficulty":21.661,"maintainability":70.494,"lintErrors":2}]
