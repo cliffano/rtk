fcc16407e366e293bae970be0267acb7c2d645ed
cliffano <cliffano@users.noreply.github.com>
2022-01-18 23:54:47 +0000

    deploy: f3528d9f57efb51fc62a7cf4f033e0b21761b64c

--- complexity/plato/files/lib_resource_types_makefile_js/report.history.js
+++ complexity/plato/files/lib_resource_types_makefile_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 18 Jan 2022 23:53:44 GMT","sloc":66,"lloc":29,"functions":5,"deliveredBugs":0.396,"difficulty":27.5,"maintainability":71.756,"lintErrors":2}]
+__history = [{"date":"Tue, 18 Jan 2022 23:54:41 GMT","sloc":66,"lloc":29,"functions":5,"deliveredBugs":0.396,"difficulty":27.5,"maintainability":71.756,"lintErrors":2}]
--- complexity/plato/files/lib_rtk_js/report.history.js
+++ complexity/plato/files/lib_rtk_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 18 Jan 2022 23:53:44 GMT","sloc":83,"lloc":32,"functions":7,"deliveredBugs":0.437,"difficulty":21.635,"maintainability":73.844,"lintErrors":2}]
+__history = [{"date":"Tue, 18 Jan 2022 23:54:41 GMT","sloc":83,"lloc":32,"functions":7,"deliveredBugs":0.437,"difficulty":21.635,"maintainability":73.844,"lintErrors":2}]
