57c9264887718cac3ff11166330c3e9813f82acf
cliffano <cliffano@users.noreply.github.com>
2023-10-12 12:51:25 +0000

    deploy: f43a97927094f5ad5a883a3bf04c364fb2a9c75d

--- complexity/plato/files/lib_resource_types_toml_js/report.history.js
+++ complexity/plato/files/lib_resource_types_toml_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Thu, 12 Oct 2023 12:50:29 GMT","sloc":54,"lloc":22,"functions":3,"deliveredBugs":0.264,"difficulty":18.088,"maintainability":71.087,"lintErrors":2}]
+__history = [{"date":"Thu, 12 Oct 2023 12:51:19 GMT","sloc":54,"lloc":22,"functions":3,"deliveredBugs":0.264,"difficulty":18.088,"maintainability":71.087,"lintErrors":2}]
