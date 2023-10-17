7e65d036beabc3a43efe9530cafc0a5e4629d4d8
cliffano <cliffano@users.noreply.github.com>
2023-10-17 13:36:08 +0000

    deploy: 825576d8798ca911fa04c668bf2e180268b1922e

--- complexity/plato/files/lib_resource_types_text_js/report.history.js
+++ complexity/plato/files/lib_resource_types_text_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 17 Oct 2023 13:26:13 GMT","sloc":53,"lloc":22,"functions":3,"deliveredBugs":0.23,"difficulty":19.2,"maintainability":70.871,"lintErrors":2}]
+__history = [{"date":"Tue, 17 Oct 2023 13:35:59 GMT","sloc":53,"lloc":22,"functions":3,"deliveredBugs":0.23,"difficulty":19.2,"maintainability":70.871,"lintErrors":2}]
