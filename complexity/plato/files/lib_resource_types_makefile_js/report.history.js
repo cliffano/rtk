ad7d0f64e1701f530f748fc4b205c7485d5e58bc
cliffano <cliffano@users.noreply.github.com>
2025-08-09 01:55:30 +0000

    deploy: 8ae80ce6f69c03e88fbb329e1d754e3fbe08ff32

--- complexity/plato/files/lib_resource_types_makefile_js/report.history.js
+++ complexity/plato/files/lib_resource_types_makefile_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 09 Aug 2025 01:54:41 GMT","sloc":66,"lloc":29,"functions":5,"deliveredBugs":0.396,"difficulty":27.5,"maintainability":71.756,"lintErrors":1}]
+__history = [{"date":"Sat, 09 Aug 2025 01:55:23 GMT","sloc":66,"lloc":29,"functions":5,"deliveredBugs":0.396,"difficulty":27.5,"maintainability":71.756,"lintErrors":1}]
